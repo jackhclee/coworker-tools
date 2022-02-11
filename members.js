04b1e57bf7f4d279682b979b30bbbfc9e841263a
Jack Lee <38123458+jackhclee@users.noreply.github.com>
2022-02-11 21:20:57 +0000

    Update members.js

--- members.js
+++ members.js
@@ -1,3 +1,9 @@
 const members = [];
 
 module.exports = members;
+
+//first name ,last name ,job title         ,department  ,manager  ,location  ,first aider ,user slack ,timezone
+//John       ,Doe       ,Software Engineer ,Engineering ,null     ,Glasgow   ,No          ,johndoe    ,America/Los_Angeles
+//Jane       ,Doe       ,Software Engineer ,Engineering ,John Doe ,Leeds     ,No          ,janedoe    ,America/Los_Angeles
+//Bob        ,Doe       ,Software Engineer ,Engineering ,Jane Doe ,Cape Town ,No          ,bobdoe     ,America/Los_Angeles
+//Sally      ,Doe       ,Software Engineer ,Engineering ,Jane Doe ,Rome      ,No          ,sallydoe   ,America/Los_Angeles
